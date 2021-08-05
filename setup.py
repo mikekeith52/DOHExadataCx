a5935978932621f72f36b6a2ca13ae9c1f01c473
Michael Keith <michaelkeith@utah.gov>
2021-08-05 12:48:36 -0600

    v 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'DOHExadataCx',
-  version = '0.1.0',
+  version = '0.1.2',
   license='MIT',        # https://help.github.com/articles/licensing-a-repository
   short_description = '',
   long_description='',
--- src/dohexadatacx/test.py
+++
@@ -1,4 +0,0 @@
-import cfg
-import pandas as pd
-
-cfg.create_config('EXADW_test_test')
